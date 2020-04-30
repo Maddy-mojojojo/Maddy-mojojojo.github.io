d3487ab61c877c12ed66b3bd2a1b6313f8011539
Maddy-mojojojo <Maddie2030@sbcglobal.net>
2020-04-30 16:33:39 -0700

    stuff

--- Guess-that-song/processGuess.php
+++ Guess-that-song/processGuess.php
@@ -2,22 +2,15 @@
     if($_SERVER["REQUEST_METHOD"] =="POST"){
         $songNameGuess = $_POST["songName"];
         $artistNameGuess = $_POST["artistName"];
-        $songName = $_POST["item".name];
-        $artistName = $_POST["item".artists];
-        $timeStamp = $_POST["timestamp"];
-
     }
-
-
-
 ?>
+
 <body> 
     <h3>Your guess:</h3>
         <p>Song Name: <?php echo $songNameGuess; ?></p>
         <p>Artist Name: <?php echo $artistNameGuess; ?></p>
-        <p>Time Elapsed: <?php echo $timeStamp; ?></p>
     <h3>Answers:</h3>
         <p>Song Name: <?php echo $songName; ?></p>
         <p>Artist Name: <?php echo $artistName; ?></p>
-        <p>Points won: <?php echo $timeStamp*10;?></p>
+        <p>Points won: <?php echo "nope";?></p>
 </body>
