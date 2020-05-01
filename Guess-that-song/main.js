67094924f4248381a84a3a9b32ee14bb2c2edfa9
Maddy-mojojojo <Maddie2030@sbcglobal.net>
2020-04-30 21:32:17 -0700

    final

--- Guess-that-song/main.js
+++ Guess-that-song/main.js
@@ -55,19 +55,20 @@ var isSongCorrect = function(song){
     }
 }
 
-var processGuess = function(artist, song){
-    var song = document.input-form.songName.value;
-    var artist = document.input-form.artistName.value;
-    if(isArtistCorrect(artist) && isSongCorrect(song)){
-        document.write("<h2>Both Answers You Gave Are Correct!</h2>");
+var processGuess = function(){
+    var x = document.getElementById("inputform0");
+    var guessSong = x.elements[1].value;
+    var guessArtist = x.elements[2].value;
+    if(isArtistCorrect(guessArtist) && isSongCorrect(guessSong)){
+        alert("<h2>Both Answers You Gave Are Correct!</h2>");
     }
-    else if(isArtistCorrect(artist) && !isSongCorrect(song)){
-        document.write("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
+    else if(isArtistCorrect(guessArtist) && !isSongCorrect(guessSong)){
+        alert("<h2>You Got The Artist Correct, But Not The Song Name!</h2>");
     }
-    else if(!isArtistCorrect(artist) && isSongCorrect(song)){
-        document.write("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
+    else if(!isArtistCorrect(guessArtist) && isSongCorrect(GuessSong)){
+        alert("<h2>You Got The Song Name Correct, But Not The Artist!</h2>");
     }
-    else if(!isArtistCorrect(artist) && !isSongCorrect(song)){
-        document.write("<h2>You Got Neither Correct!</h2>");
+    else if(!isArtistCorrect(guessArtist) && !isSongCorrect(guessSong)){
+        alert("<h2>You Got Neither Correct!</h2>");
     }
 }
